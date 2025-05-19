dc045ee6ae3faf5e952e6d643ddccdcc3c0cb034
Peter Keogh <peterkeoghdev@gmail.com>
2025-05-19 18:22:38 +0100

    Fix typo

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -2,7 +2,7 @@ class Lab < ApplicationRecord
   enum :status, {
     idea: "idea",
     planned: "planned",
-    in_progress: "in_progress",
+    in_progress: "in-progress",
     completed: "completed"
   }
 end
